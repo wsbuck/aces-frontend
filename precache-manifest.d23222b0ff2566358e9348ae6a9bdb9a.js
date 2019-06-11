3bde89383d548edb62ea8a3f6897181af5c0094e
William Buck <wsbuckiii@gmail.com>
2019-06-11 16:26:00 -0700

    Updates

---
+++ precache-manifest.d23222b0ff2566358e9348ae6a9bdb9a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce9887b055a61775b69f97e5f804259e",
+    "url": "/rotopope/index.html"
+  },
+  {
+    "revision": "d0453e6391a4092b9472",
+    "url": "/rotopope/static/css/main.94d46676.chunk.css"
+  },
+  {
+    "revision": "a72e4ba90324728335c6",
+    "url": "/rotopope/static/js/2.2ee40941.chunk.js"
+  },
+  {
+    "revision": "d0453e6391a4092b9472",
+    "url": "/rotopope/static/js/main.85f6e64f.chunk.js"
+  },
+  {
+    "revision": "e16e027ae00fb6172fce",
+    "url": "/rotopope/static/js/runtime~main.4f6df7dd.js"
+  }
+]);
