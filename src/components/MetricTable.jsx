a87dabc7a7b95be38f4e56c1de9540d7e0d8e8ba
William Buck <wsbuckiii@gmail.com>
2019-06-28 14:41:09 -0700

    formatting for MetricTable

--- src/components/MetricTable.jsx
+++ src/components/MetricTable.jsx
@@ -14,17 +14,17 @@ export default function MetricTable(props) {
           </tr>
           <tr>
             <td>ACES</td>
-            <td>{playerMetrics['ACES']['value'].toFixed(2)}</td>
+            <td>{(playerMetrics['ACES']['value'] * 100).toFixed(0) + '%'}</td>
             <td>{(playerMetrics['ACES']['percentile'] * 100).toFixed(0) + '%'}</td>
           </tr>
           <tr>
             <td>Whiffs%</td>
-            <td>{playerMetrics['Whiffs']['value'].toFixed(2)}</td>
+            <td>{(playerMetrics['Whiffs']['value'] * 100).toFixed(0) + '%'}</td>
             <td>{(playerMetrics['Whiffs']['percentile'] * 100).toFixed(0) + '%'}</td>
           </tr>
           <tr>
             <td>CSW%</td>
-            <td>{playerMetrics['CSW']['value'].toFixed(2)}</td>
+            <td>{(playerMetrics['CSW']['value'] * 100).toFixed(0) + '%'}</td>
             <td>{(playerMetrics['CSW']['percentile'] * 100).toFixed(0) + '%'}</td>
           </tr>
         </tbody>
